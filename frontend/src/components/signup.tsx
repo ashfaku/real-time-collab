0f63259f2f0d2a699c9040d3f2eb78658fdcd4b4
Ashfak Uddin <ashmanpapet@gmail.com>
2025-09-24 19:41:13 -0400

    connected front to back and back to sql

--- frontend/src/components/signup.tsx
+++ frontend/src/components/signup.tsx
@@ -9,21 +9,37 @@ const SignUp: React.FC<DefaultSignUpProps> = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    const handleSignUp = () => {
-      let accountJSON = {
-        username: username,
-        password: password,
-        email: email
-      };
-      console.log(accountJSON);
-    };
+    const handleSignUp = async (e) => {
+        console.log("sendsdsing");
+        e.preventDefault();
+        try {
+            // Using fetch API
+            console.log("sending");
+            const response = await fetch('http://localhost:5000/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    email: email
+                }),
+            });
+            const data = await response.json();
+            console.log(response);
+        } catch (error) {
+            console.error('Error sending data:', error);
+            // Handle error (e.g., show error message)
+        }
+    }
     return (
         <div id = "loginbox">
             <h1>Sign Up</h1>
             <input type="text" id="username" name="username" placeholder="Enter your username..." onChange={(e) => setUsername(e.target.value)} />
-            <input type="email" id="email" name="email" placeholder="Enter your email..." onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" id="email" name="email" placeholder="Enter your email..." onChange={(e) => setEmail(e.target.value)} />
             <input type="text" id="password" name="password" placeholder="Enter your password..." onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleSignUp}>Login</button>
+            <button onClick={handleSignUp}>Sign Up</button>
         </div>
     );
 };
